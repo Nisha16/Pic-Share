1b9437910be4229dc60b2e67df9c3da903d35c9d
Vidya <vidyags@outlook.com>
2016-08-04 16:07:29 -0700

    initial commit includes store creation

---
+++ client/data/comments.js
@@ -0,0 +1,303 @@
+const comments = {
+  "BAhvZrRwcfu":[
+    {
+      "text":"Totally need to try this.",
+      "user": "amazingneil"
+    }
+  ],
+  "BAcyDyQwcXX":[
+    {
+      "text":"anil. WE should have lunch.",
+      "user": "nisha"
+    },
+    {
+      "text":"#adults",
+      "user": "nisha"
+    },
+    {
+      "text":"@nisha yes!",
+      "user": "anil"
+    },
+    {
+      "text":"😍 love Hamilton!",
+      "user": "vidya"
+    }
+  ],
+  "BAPIPRjQce9":[
+    {
+      "text":"Those are cute! They're like silver dollar pancakes.",
+      "user": "neil"
+    },
+    {
+      "text":"I like baby pancakes but gluten free please!! I'll bring the coffee!! See you in 6 days!!!!!! 😝😛😝♥️",
+      "user": "aria"
+    },
+    {
+      "text":"... and apparently growing baby. 👀. Yum.",
+      "user": "ezra"
+    },
+    {
+      "text":"@anil 👍 my daughter is a pancake eating machine!",
+      "user": "vidya"
+    },
+    {
+      "text":"Nice stove!",
+      "user": "nisha"
+    },
+    {
+      "text":"Genius bottle use! Do you make a single batch of batter or make a lot and freeze it?",
+      "user": "crazy_star"
+    },
+    {
+      "text":"@crazy_star I just made a batch and put in in a FIFO bottle. Keeps in the fridge for a few days.",
+      "user": "anil"
+    },
+    {
+      "text":"@crazy_star thanks! It's a Jenn air - so nice to cool with!",
+      "user": "anil"
+    },
+    {
+      "text":"Where would you go and for how long, if you had location freedom? - Greg 🌎",
+      "user": "world_greg"
+    }
+  ],
+  "BAF_KY4wcRY":[
+    {
+      "text":"Looking great anil! I'd like to see the other side of the room too.",
+      "user": "rithin"
+    },
+    {
+      "text":"I've never caught your podcast. Have one right? Btw - they don't have a Canary pillow? 😝",
+      "user": "rithin"
+    },
+    {
+      "text":"Great way to start the year.",
+      "user": "rithin"
+    },
+    {
+      "text":"Are there 4k monitors?",
+      "user": "rithin"
+    },
+    {
+      "text":"@nish that is where I put all the junk. I'll have to clean that side too @henrihelvetica no podcast yet! @pmgllc ohh yeah! @alexbaumgertner yep - the main one is 4K - I'm loving it",
+      "user": "anil"
+    },
+    {
+      "text":"That chrome pillow. 😉",
+      "user": "rithin"
+    },
+    {
+      "text":"@anil is that the Dell 4k? The MacBook Pro powers it well? I also have a Retina™ / x1 setup as well. Very handy.",
+      "user": "nisha"
+    },
+    {
+      "text":"#minimalsetups",
+      "user": "anil"
+    }
+  ],
+  "_4jHytwcUA":[
+    {
+      "text":"that is the sound of success!",
+      "user": "crazy_star"
+    }
+  ],
+  "_zbaOlQcbn":[
+    {
+      "text":"Did she get to eat her letter?",
+      "user": "myself_anil"
+    },
+    {
+      "text":"Nope @myself_anil! She has too much teeth now (8) so that would definitely be her first cavity 😉",
+      "user": "nisha"
+    }
+  ],
+  "_rmvQfQce8":[
+    {
+      "text":"A+",
+      "user": "vid_me"
+    },
+    {
+      "text":"I recently went to a ramen place in Philly. So amazing!",
+      "user": "vid_me"
+    }
+  ],
+  "_ep9kiQcVy":[
+    {
+      "text":"All bundled up!  Where ya goin?",
+      "user": "nishhh.star"
+    }
+  ],
+  "_XpJcrwcSn":[
+    {
+      "text":"Super congrats! That's wicked cool and looks great.",
+      "user": "nishhh"
+    },
+    {
+      "text":"real live paper magazine? woah haha. flex box is aanilome though, could rage quit without it",
+      "user": "nishhh"
+    },
+    {
+      "text":"@nishhh haha yes! Old school stylez!",
+      "user": "anil"
+    }
+  ],
+  "_KnU7MwceA":[
+
+  ],
+  "_HMejJQcY5":[
+    {
+      "text":"👌",
+      "user": "bestofme_ani"
+    },
+    {
+      "text":"Nice shot, mister!",
+      "user": "bestofme_ani"
+    }
+  ],
+  "_Fq2zmwcaz":[
+    {
+      "text":"😍",
+      "user": "bestofme_ani"
+    },
+    {
+      "text":"Very cool!",
+      "user": "crazy_star"
+    }
+  ],
+  "_A2r0aQcfD":[
+    {
+      "text":"Uhu!",
+      "user": "vid_me"
+    }
+  ],
+  "1rhFawccs":[
+    {
+      "text":"What's your lighting source?",
+      "user": "rithin"
+    },
+    {
+      "text":"And what are you using for XLR mix adapter? I found a big price jump between $55 range and $170 range.",
+      "user": "pmgllc"
+    },
+    {
+      "text":"I still need a desk boom for mine mic. Nice upgrades",
+      "user": "stolinski"
+    }
+  ],
+  "pjx-gQcVi":[
+
+  ],
+  "oTZ0zQcWt":[
+    {
+      "text":"Love the coat! Where's it from? Lol",
+      "user": "_lindersss"
+    }
+  ],
+  "mxKQoQcQh":[
+
+  ],
+  "hZh6IQcfN":[
+    {
+      "text":"What do we gotta do to get some :)? @anil",
+      "user": "jonasbad"
+    },
+    {
+      "text":"Might drop by today - real quick. Trade you an illegal pin for these? Lol. @anil",
+      "user": "henrihelvetica"
+    },
+    {
+      "text":"I'm with @jonasbad on this. What we gotta do? :D",
+      "user": "datamoshr"
+    },
+    {
+      "text":"@jonasbad @datamoshr I'll post them up on my blog soon!",
+      "user": "anil"
+    },
+    {
+      "text":"Want",
+      "user": "bestofme_ani"
+    },
+    {
+      "text":"want!",
+      "user": "anil.myself"
+    },
+    {
+      "text":"#Top",
+      "user": "nishhh.me"
+    }
+  ],
+  "fasqlQceO":[
+    {
+      "text":"Where's lux at? 💤?",
+      "user": "ani"
+    },
+    {
+      "text":"Love this house during the holidays! And all other times of the year...",
+      "user": "mallpur"
+    }
+  ],
+  "VBgtGQcSf":[
+    {
+      "text":"@neha",
+      "user": "nish"
+    },
+    {
+      "text":"Next on my list!",
+      "user": "vid"
+    },
+    {
+      "text":"Is that from collective arts ?",
+      "user": "honey"
+    }
+  ],
+  "FpTyHQcau":[
+    {
+      "text":"@nisha  that vest!!! 😍",
+      "user": "anil"
+    },
+    {
+      "text":"I just love her! @nisha",
+      "user": "anil"
+    },
+    {
+      "text":"@neil I know! My friend gave it to her and I wanted a matching one but only Lux can pull it off. She's so fancy 😉",
+      "user": "nisha"
+    },
+    {
+      "text":"Char has that vest!!! Super cute!",
+      "user": "anil"
+    },
+    {
+      "text":"You'll have to meet her soon @kalibrix!!",
+      "user": "nisha"
+    },
+    {
+      "text":"Haha @nisha so true, babies these days are sooo stylish. 😎",
+      "user": "sowmya"
+    },
+    {
+      "text":"JavaScript 😄😆🙋",
+      "user": "lucas"
+    },
+    {
+      "text":"That hoodie is amazing! Where did you get it?",
+      "user": "chaitra"
+    },
+    {
+      "text":"@br11x I did a teespring a few months ago - maybe I'll do another one soon",
+      "user": "anil"
+    }
+  ],
+  "B3eiIwcYV":[
+    {
+      "text":"If you get in the mood for authentic Italian pizza, check out @backspaceaustin - it's👌🏻",
+      "user": "sowmya"
+    },
+    {
+      "text":"😱 jealous",
+      "user": "nishanth"
+    }
+  ]
+};
+
+export default comments;
